a87c8864d064113008c119e217e7d7ad05d26fe9
SAPPA SAI <sappasai.msccs@gmail.com>
2025-06-08 20:15:39 +0530

    Update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -1,6 +1,16 @@
-from app import app  # or however you import your Flask app
+import sys
+import os
+from dotenv import load_dotenv
 
-if __name__ == "__main__":
-    app.run()
+# Update this with your actual project directory
+project_home = '/home/sappasai/TAN_python_project'
 
-application = app
+# Add your project folder to the Python path
+if project_home not in sys.path:
+    sys.path.insert(0, project_home)
+
+# Load environment variables from .env file
+load_dotenv(os.path.join(project_home, '.env'))
+
+# Set the Flask app module
+from app import app as application  # Assuming your Flask app is named app.py and app = Flask(__name__)
